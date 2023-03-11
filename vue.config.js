96747fa478334f88a9fb42f5d7fb9287cfd36d84
Hayk Hunanyan <haykhuna@gmail.com>
2023-03-11 17:56:43 +0400

    fix build 10m

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-})
+const ScriptSetup = require('unplugin-vue2-script-setup/webpack').default
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
+  parallel: false, // disable thread-loader, which is not compactible with this plugin
+  configureWebpack: {
+    plugins: [ScriptSetup({})],
+  },
+}
